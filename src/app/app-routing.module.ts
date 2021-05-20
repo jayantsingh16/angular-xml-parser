890bd2ce69d018748aab08c075327c401cd433ef
jayantsingh16 <jayant.singh@nl.abnamro.com>
2021-05-20 09:38:35 +0200

    4 files modified, 9 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { Xml0Component } from './xml0/xml0.component';
+import { Xml1Component } from './xml1/xml1.component';
+import { Xml2Component } from './xml2/xml2.component';
+
+const routes: Routes = [
+  { path: 'xml0', component: Xml0Component },
+  { path: 'xml1', component: Xml1Component },
+  { path: 'xml2', component: Xml2Component }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Xml0Component } from './xml0/xml0.component';
+import { Xml1Component } from './xml1/xml1.component';
+import { Xml2Component } from './xml2/xml2.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Xml0Component,
+    Xml1Component,
+    Xml2Component
   ],
   imports: [
     BrowserModule,
